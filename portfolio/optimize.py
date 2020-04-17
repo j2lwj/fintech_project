de10153c386b3343002248455c190d529d600543
Jeremy Ling Wei Jie <j2lwj@hotmail.com>
2020-04-17 23:59:16 +0800

    pushing optimize.py and reference files

---
+++ portfolio/optimize.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pypfopt import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
+
+df = pd.read_csv('latest_stock_prices.csv',parse_dates=True, index_col="date")
+
+data = df[df.columns.intersection(idArr)]
+data.dtypes
+
+mu = expected_returns.mean_historical_return(data)
+S = risk_models.sample_cov(data)
+
+# Optimise for maximal Sharpe ratio
+ef = EfficientFrontier(mu, S)
+raw_weights = ef.max_sharpe()
+cleaned_weights = ef.clean_weights()
+
+ef.save_weights_to_file("weights.csv")  # saves to file
+print(cleaned_weights)
+ef.portfolio_performance(verbose=True)
+
